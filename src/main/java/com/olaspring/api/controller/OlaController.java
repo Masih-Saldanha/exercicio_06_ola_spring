4ed41e2cd88f08f54fd48c204fc73b0663eef126
Masih <masih.saldanha@gmail.com>
2023-01-19 14:37:07 -0300

    feat: add GET '/ola' endpoint

---
+++ src/main/java/com/olaspring/api/controller/OlaController.java
@@ -0,0 +1,14 @@
+package com.olaspring.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class OlaController {
+    @GetMapping("/ola")
+    public String olaSpring() {
+        return "Olá Spring";
+    }
+}
